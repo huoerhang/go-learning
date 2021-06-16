1ebe5a8e4e634ad296e66de5841a7612700677f5
不正经码农 <huohaihang@126.com>
2021-06-16 18:36:45 +0800

    defer FizzBuzz

---
+++ FizzBuzz/main.go
@@ -0,0 +1,26 @@
+﻿package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	for num := 1; num <= 100; num++ {
+		fmt.Println(fizzbuzz(num))
+	}
+}
+
+func fizzbuzz(num int) string {
+	switch {
+	case num%15 == 0:
+		return "FizzBuzz"
+
+	case num%3 == 0:
+		return "Fizz"
+	case num%5 == 0:
+		return "Buzz"
+	default:
+		return strconv.Itoa(num)
+	}
+}
---
+++ defer/main.go
@@ -0,0 +1,27 @@
+﻿package main
+
+//import "fmt"
+import (
+	"io"
+	"os"
+)
+
+func main() {
+	f, err := os.Create("notes.txt")
+
+	if err != nil {
+		return
+	}
+
+	defer f.Close()
+
+	if _, err = io.WriteString(f, "Learning GO!"); err != nil {
+		return
+	}
+
+	f.Sync()
+	// for i := 1; i <= 4; i++ {
+	// 	defer fmt.Println("deferred", -i)
+	// 	fmt.Println("regular", i)
+	// }
+}
